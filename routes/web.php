4568d80cb64a1afc55731ad9f0571806d1f88a50
Muneeb1Geek <m.muneebulrahman@gmail.com>
2025-02-17 19:47:31 +0500

    route removed

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/linkstorage', function () {
-    Artisan::call('storage:link');
-});
-
 require __DIR__.'/auth.php';
