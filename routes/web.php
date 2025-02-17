93ebf4f8065044283eeafd6fac0c0b67d29b8454
Muneeb1Geek <m.muneebulrahman@gmail.com>
2025-02-17 19:41:09 +0500

    storage link

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', function () {
@@ -12,6 +13,10 @@ Route::middleware('auth')->group(function () {
     Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::post('/update-password', [ProfileController::class, 'updatePassword'])->name('update.password');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/linkstorage', function () {
+    Artisan::call('storage:link');
 });
 
 require __DIR__.'/auth.php';
